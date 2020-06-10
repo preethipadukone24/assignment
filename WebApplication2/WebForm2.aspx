d8cfcef92914c78905807a2664ff93df39d99e71
preethipadukone24@gmail.com <preethipadukone24@gmail.com>
2020-06-10 13:20:46 +0530

    Add project files.

---
+++ WebApplication2/WebForm2.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication2.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:Label ID="lbllat" runat="server" Text="Latitude"></asp:Label>
+        <asp:TextBox ID="txtlat" runat="server"></asp:TextBox>
+        <p>
+            <asp:Label ID="lbllong" runat="server" Text="Longitude"></asp:Label>
+            <asp:TextBox ID="txtlong" runat="server"></asp:TextBox>
+        </p>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Get Data" />
+        <p>
+            <asp:Label ID="lblError" runat="server" Text=""></asp:Label>
+        </p>
+        <p>
+            Sunrise is at : 
+            <asp:Label ID="lblsunrise" runat="server" Text=""></asp:Label>
+        </p>
+        <p>
+            Sunrise is at : 
+            <asp:Label ID="lblsunset" runat="server" Text=""></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebApplication2/WebForm2.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+using System.Net.Http;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Configuration;
+using Newtonsoft.Json.Linq;
+
+namespace WebApplication2
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (txtlat.Text.Length == 0 || txtlong.Text.Length == 0)
+            {
+                lblError.Text = "Please provide latitude longitude in correct format.";
+            }
+            else
+            {
+                lblError.Text = "";
+                String callingResource = "json?lat=" + txtlat.Text + "&lng=" + txtlong.Text + "&date=today";
+
+                using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+                {
+                    client.BaseAddress = new Uri("https://api.sunrise-sunset.org/");
+                    HttpResponseMessage response = client.GetAsync(callingResource).Result;
+                    response.EnsureSuccessStatusCode();
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("Result: " + result);
+                    JObject joResponse = JObject.Parse(result);
+                    JObject ojObject = (JObject)joResponse["results"];
+
+                    lblsunrise.Text = (String)ojObject["sunrise"];
+                    lblsunset.Text = (String)ojObject["sunset"];
+                }
+            }
+        }
+
+    }
+}
